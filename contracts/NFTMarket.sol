0b62a3d15b12c2899e297d624f263c1cc4ade426
Hardy Joysan <hardyjoysan@gmail.com>
2022-11-30 15:31:59 +0530

    fetchCreated NFTs function

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -139,4 +139,29 @@ contract NFTMarket is ReentrancyGuard {
 
     return items;
   }
+
+  function fetchItemsCreated() public view returns (MarketItem[] memory) {
+    uint256 totalItemCount = _itemIds.current();
+    uint256 itemCount = 0;
+    uint256 currentIndex = 0;
+
+    for (uint256 i = 0; i < totalItemCount; i++) {
+      if (idToMarketItem[i + 1].seller == msg.sender) {
+        itemCount += 1;
+      }
+    }
+
+    MarketItem[] memory items = new MarketItem[](itemCount);
+
+    for (uint256 i = 0; i < totalItemCount; i++) {
+      if (idToMarketItem[i + 1].seller == msg.sender) {
+        uint256 currentId = idToMarketItem[i + 1].itemId;
+        MarketItem storage currentItem = idToMarketItem[currentId];
+        items[currentIndex] = currentItem;
+        currentIndex += 1;
+      }
+    }
+
+    return items;
+  }
 }
