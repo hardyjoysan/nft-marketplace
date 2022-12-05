e4496e5252e0f73bdc198a46a07874d181763818
Hardy Joysan <hardyjoysan@gmail.com>
2022-12-05 18:55:18 +0530

    creator dashboard page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,16 +2,15 @@ import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Web3Modal from "web3modal";
-import { useRouter } from "next/router";
 
 import { nftaddress, nftmarketaddress } from "../config";
 import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
 import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
 
-export default function MyNfts() {
+export default function Dashboard() {
   const [nfts, setNfts] = useState([]);
+  const [sold, setSold] = useState([]);
   const [loadingState, setLoadingState] = useState("not-loaded");
-  const router = useRouter();
 
   useEffect(() => {
     loadNFTs();
@@ -29,38 +28,39 @@ export default function MyNfts() {
       Market.abi,
       signer
     );
-    const data = await MarketContract.fetchMyNFTs();
+    const data = await MarketContract.fetchItemsCreated();
 
     const items = await Promise.all(
       data.map(async (i) => {
-        const tokenURI = await NFTcontract.tokenURI(i.tokenId);
-        const meta = await axios.get(tokenURI);
+        const tokenUri = await NFTcontract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
         let price = ethers.utils.formatUnits(i.price.toString(), "ether");
         let item = {
           price,
           tokenId: i.tokenId.toNumber(),
           seller: i.seller,
           owner: i.owner,
+          sold: i.sold,
           image: meta.data.image,
-          tokenURI,
         };
         return item;
       })
     );
+
+    const soldItems = items.filter((i) => i.sold);
+
     setNfts(items);
+    setSold(soldItems);
     setLoadingState("loaded");
   }
 
-  function listNFT(nft) {
-    console.log("nft:", nft);
-    router.push(`/resell-nft?id=${nft.tokenId}&tokenURI=${nft.tokenURI}`);
-  }
-
   if (loadingState === "loaded" && !nfts.length)
-    return <h1 className="py-10 px-20 text-3xl">No NFTs owned</h1>;
+    return <h1 className="py-10 px-20 text-3xl">No NFTs listed</h1>;
+
   return (
-    <div className="flex justify-center">
+    <div>
       <div className="p-4">
+        <h2 className="text-2xl py-2">Items Created</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {nfts.map((nft, i) => (
             <div key={i} className="border shadow rounded-xl overflow-hidden">
@@ -69,16 +69,32 @@ export default function MyNfts() {
                 <p className="text-2xl font-bold text-white">
                   Price - {nft.price} Eth
                 </p>
-                <button
-                  className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
-                  onClick={() => listNFT(nft)}
-                >
-                  List
-                </button>
               </div>
             </div>
           ))}
         </div>
+      </div>
+      <div className="p-4">
+        {Boolean(sold.length) && (
+          <>
+            <h2 className="text-2xl py-2">Items Sold</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+              {sold.map((nft, i) => (
+                <div
+                  key={i}
+                  className="border shadow rounded-xl overflow-hidden"
+                >
+                  <img src={nft.image} className="rounded" />
+                  <div className="p-4 bg-black">
+                    <p className="text-2xl font-bold text-white">
+                      Price - {nft.price} Eth
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
